d00628acef2375a919e7bf844108e8850b1b4c9a
ILYA ERSHOV <ershov.ilya@gmail.com>
2016-02-15 12:49:03 +0300

    scaffolds

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+ /**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 15.02.2016
+ * Time: 12:48
+ */
+
+$start =microtime( true) ;
+header( 'Content-Type: text/plain; charset=utf-8' ) ;
+//if(isset($_GET['t']))
+define( 'DEBUG' , true ) ;
+defined( 'DEBUG') or define('DEBUG' , false) ;
+
+if( DEBUG ){
+    error_reporting( E_ERROR | E_WARNING ) ;
+    ini_set( "display_errors" , 1 ) ;
+}
+
+print "OK";
